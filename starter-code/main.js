515841b9a2da3bd3067e963bcb8c1a846342a894
Kathryn Wood <kathrynwood@kathryns-mbp.attlocal.net>
2016-10-26 13:05:46 -0400

    Added js conditionals

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,22 @@ var cardOne = "Queen";
 var cardTwo = "King";
 var cardThree = "King";
 var cardFour = "Queen";
+
+
+if (cardTwo === cardFour) {
+	alert("You found a match!");
+} else {
+	alert ("Sorry, try again.");
+}
+
+if (cardOne === cardFour) {
+	alert ("You found a match!");
+} else {
+	alert ("Sorry, try again.");
+}
+
+if (cardTwo === cardThree) {
+	alert ("You found a match!");
+}	else {
+	alert ("Sorry, try again.");
+}
